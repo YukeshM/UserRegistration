21b958e9c1b6dcb0c4940258a3e847b5b1fa42a3
Codetekkens <codetekken@gmail.com>
2025-01-18 00:32:26 +0530

    changed the folder structure

--- user-registration/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './user/login/login.component';
-import { RegistrationComponent } from './user/registration/registration.component';
-
-export const routes: Routes = [ 
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegistrationComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
-];
