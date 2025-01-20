6261b6a5c2081488962603d122dc10771000310d
Codetekkens <codetekken@gmail.com>
2025-01-20 11:45:41 +0530

    added validator

--- Services/UserRegistrationService/UserRegistrationService.Model/Validator/RegisterModelValidator.cs
+++ Services/UserRegistrationService/UserRegistrationService.Model/Validator/RegisterModelValidator.cs
@@ -18,12 +18,12 @@ namespace UserRegistrationService.Core.Validator
         {
             RuleFor(x => x.Username)
                 .NotEmpty().WithMessage("Username is required")
-                .Length(4, 20).WithMessage("Username must be between 4 and 20 characters")
+                .Length(3, 50).WithMessage("Username must be between 4 and 20 characters")
                 .Matches("^[a-zA-Z0-9]*$").WithMessage("Username can only contain letters and numbers");
 
             RuleFor(x => x.Password)
                 .NotEmpty().WithMessage("Password is required")
-                .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
+                .MinimumLength(6).WithMessage("Password must be at least 8 characters long")
                 .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
                 .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter")
                 .Matches("[0-9]").WithMessage("Password must contain at least one number")
